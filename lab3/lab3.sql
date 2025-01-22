f719cad617d84ba00a2ab8ab0caa045cff65456e
Claire Do <claire.do@snowflake.com>
2025-01-21 23:03:56 -0500

    Update lab3.sql

--- lab3/lab3.sql
+++ lab3/lab3.sql
@@ -103,6 +103,20 @@ ALTER WAREHOUSE query_wh SUSPEND;
 ALTER WAREHOUSE query_wh SET warehouse_size = XSMALL;
 
 -- Run the Pricing Summary Report Query (Q1) and take note of the query duration (this may take about 1m 30s)
+SELECT l_returnflag,
+       l_linestatus,
+       sum(l_quantity) as sum_qty,
+       sum(l_extendedprice) as sum_base_price,
+       sum(l_extendedprice * (1-l_discount)) as sum_disc_price,
+       sum(l_extendedprice * (1-l_discount) * (1+l_tax)) as sum_charge,
+       avg(l_quantity) as avg_qty,
+       avg(l_extendedprice) as avg_price,
+       avg(l_discount) as avg_disc,
+       count(*) as count_order
+ FROM lineitem
+ WHERE l_shipdate <= dateadd(day, -90, to_date('1998-12-01'))
+ GROUP BY l_returnflag,l_linestatus
+ ORDER BY l_returnflag, l_linestatus;
 
 -- suspend the warehouse
 ALTER WAREHOUSE query_wh SUSPEND;
@@ -111,6 +125,20 @@ ALTER WAREHOUSE query_wh SUSPEND;
 ALTER WAREHOUSE query_wh SET warehouse_size = MEDIUM;
 
 -- Run the Pricing Summary Report Query (Q1) and take note of the query duration (this may take about 30s)
+SELECT l_returnflag,
+       l_linestatus,
+       sum(l_quantity) as sum_qty,
+       sum(l_extendedprice) as sum_base_price,
+       sum(l_extendedprice * (1-l_discount)) as sum_disc_price,
+       sum(l_extendedprice * (1-l_discount) * (1+l_tax)) as sum_charge,
+       avg(l_quantity) as avg_qty,
+       avg(l_extendedprice) as avg_price,
+       avg(l_discount) as avg_disc,
+       count(*) as count_order
+ FROM lineitem
+ WHERE l_shipdate <= dateadd(day, -90, to_date('1998-12-01'))
+ GROUP BY l_returnflag,l_linestatus
+ ORDER BY l_returnflag, l_linestatus;
 
 -- scale-down back to XSMALL for the next lab
 ALTER WAREHOUSE query_wh SET warehouse_size = XSMALL;
